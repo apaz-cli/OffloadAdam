653e0f1a33430627df311ae50191e18404ab64eb
apaz-cli <aarpazdera@gmail.com>
2025-02-05 13:29:28 -0600

    feat: Implement CPUAdam optimizer with parameter construction function

--- cpu_adam.py
+++ cpu_adam.py
@@ -1,12 +1,29 @@
+from typing import Iterable
 import torch
 import offload_adam
 
 class CPUAdam:
-    def __init__(self):
+    def __init__(self, opt):
         pass
     def step():
         pass
     def zero_grad(self, set_to_none = True):
-        return super().zero_grad(set_to_none)
+        pass
     def __del__(self):
         pass
+
+
+def construct_for_parameters(params: Iterable[torch.Tensor] | torch.Tensor, lr=1e-3, betas=(0.9, 0.999), eps=1e-8, weight_decay=0, amsgrad=False) -> list[CPUAdam]:
+    beta1, beta2 = betas
+
+    opts: list[CPUAdam] = []
+    params = (params,) if isinstance(params, torch.Tensor) else params
+    for p in params:
+        assert p.grad is not None
+        assert p.grad.device.type == 'cpu'
+
+        off_opt = offload_adam.create_optimizer(p.grad, lr, beta1, beta2, eps, weight_decay, amsgrad)
+        opt = CPUAdam(off_opt)
+        opts.append(opt)
+
+    return opts
